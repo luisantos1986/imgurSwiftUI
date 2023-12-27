88fe09c2d06142464395ba5d9a2effe8ff3444b6
JibJabLuisantos <luis.santos@jibjab.com>
2023-12-27 14:18:50 -0600

    Initial Commit

---
+++ SocialNetworkExcecise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialNetworkExcecise
+//
+//  Created by Luis Eugenio Santos Gonzalez on 27/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialNetworkExcecise/SocialNetworkExceciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkExceciseApp.swift
+//  SocialNetworkExcecise
+//
+//  Created by Luis Eugenio Santos Gonzalez on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkExceciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
